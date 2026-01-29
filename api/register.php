cd5d7edca77e5dc1fa9b861a982d31f14aedfa23
Andres <mr608040@gmail.com>
2026-01-29 11:10:56 -0500

    chore: drive disable debug in production

--- api/register.php
+++ api/register.php
@@ -2,12 +2,7 @@
 // register.php - registro de usuario
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
