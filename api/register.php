3b30261deb27cd804f4b408ccc8b13dab25444cd
Andres <mr608040@gmail.com>
2026-01-29 11:04:09 -0500

    chore: enable debug mode in register.php

--- api/register.php
+++ api/register.php
@@ -5,6 +5,10 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
